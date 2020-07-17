f45b87a0ef3df7ea2e6ffb97e5365755ccfdea1f
ventsi <v.saraminev@abv.bg>
2020-07-17 21:18:05 +0300

    Working on user input functionality

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
+const readline = require('readline');
+const readlineInterface = readline.createInterface({
+   input: process.stdin,
+   output: process.stdout,
+});
 const RED_CELL = 0;
 const GREEN_CELL = 1;
 let greenGenerations = 0;
+const questions = [
+   'Size of the grid:',
+   'Generation Zero State:',
+   'Coordinates and number N:',
+];
+
 //Hard coded 2D array for test
 
 let matrix = [
@@ -15,31 +26,51 @@ let newMatrix = [
    [0, 0, 0],
 ];
 
-for (let index = 0; index < 10; index++) {
-   for (let row = 0; row < matrix.length; row++) {
-      for (let column = 0; column < matrix[row].length; column++) {
-         const neighbors = findAllNeighbors(matrix, row, column);
-         const greenNeighbors = getSumOfEqualCells(neighbors, GREEN_CELL);
-         const redNeighbors = getSumOfEqualCells(neighbors, RED_CELL);
-         const currentCell = matrix[row][column];
-         const nextGenerationCell = createNextGeneration(
-            currentCell,
-            greenNeighbors,
-            redNeighbors
-         );
-         newMatrix[row][column] = nextGenerationCell;
-         if (row === 0 && column === 1 && nextGenerationCell === GREEN_CELL) {
-            greenGenerations++;
+start();
+
+async function start() {
+   const grid = await ask(questions[0]);
+   const numbers = grid.split(',');
+   const width = Number(numbers[0]);
+   const heigh = Number(numbers[1]);
+   let arr = [];
+   for (let index = 0; index < heigh; index++) {
+      let result = await ask(questions[1]);
+      arr.push(result);
+   }
+   let coordinates = await ask(questions[2]);
+
+   for (let index = 0; index < 10; index++) {
+      for (let row = 0; row < matrix.length; row++) {
+         for (let column = 0; column < matrix[row].length; column++) {
+            const neighbors = findAllNeighbors(matrix, row, column);
+            const greenNeighbors = getSumOfEqualCells(neighbors, GREEN_CELL);
+            const redNeighbors = getSumOfEqualCells(neighbors, RED_CELL);
+            const currentCell = matrix[row][column];
+            const nextGenerationCell = createNextGeneration(
+               currentCell,
+               greenNeighbors,
+               redNeighbors
+            );
+            newMatrix[row][column] = nextGenerationCell;
+            if (
+               row === 0 &&
+               column === 1 &&
+               nextGenerationCell === GREEN_CELL
+            ) {
+               greenGenerations++;
+            }
          }
       }
-   }
 
-   matrix = newMatrix;
-   newMatrix = [
-      [0, 0, 0],
-      [0, 0, 0],
-      [0, 0, 0],
-   ];
+      matrix = newMatrix;
+      newMatrix = [
+         [0, 0, 0],
+         [0, 0, 0],
+         [0, 0, 0],
+      ];
+   }
+   process.exit();
 }
 
 function findAllNeighbors(matrix, row, column) {
@@ -78,4 +109,10 @@ function createNextGeneration(cell, greens, reds) {
       }
       return RED_CELL;
    }
+}
+
+function ask(questionText) {
+   return new Promise((resolve, reject) => {
+      readlineInterface.question(questionText, (input) => resolve(input));
+   });
 }
